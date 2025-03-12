642b5ac7853cadf0e728434ba7fcb950d016886d
rebekklj <rebekklj@stud.ntnu.no>
2025-03-12 11:16:45 +0100

    Utvidet data_reader funksjonen. Klar til å testes.

--- proj_environment-main/Mappe_del1/rebekka_1.py
+++ proj_environment-main/Mappe_del1/rebekka_1.py
@@ -7,15 +7,48 @@ import os
 import pandas as pd
 import sys
 import time
+import requests
+import tempfile
+import shutil
 
-"""Fra forelesning: Så langt som mulig, er det lurt å forutse hvilke
-deler av programmet som kan feile, og prøve om
-vi kan få det til å kræsje der på en planlagt måte"""
 
-#File path
-file_name = '2010_2020_rainfall.csv'
+def download_temp_file(url):
+    """
+    Laster ned en fil fra URL og lagrer den midlertidig.
+    Returnerer banen til den midlertidige filen.
+    """
+    startTime = time.time()
+    tmp_file = tempfile.NamedTemporaryFile(delete=False, suffix=os.path.splitext(url)[1])
+    try:
+        with requests.get(url, stream=True) as r:
+            r.raise_for_status()
+            # Skriv data i biter for å håndtere store filer
+            for chunk in r.iter_content(chunk_size=8192):
+                tmp_file.write(chunk)
+    finally:
+        tmp_file.close()
+    endTime = time.time()
+    tot_time = endTime - startTime
+    print('The download_temp_file code took ', tot_time, ' seconds to run')
+    return tmp_file.name
 
-def data_reader(filename):
+
+def data_reader(filename, nanlimit):
+    startTime = time.time()
+    """Leser strukturen og informasjonen til en fil.
+    Filen kan være av typen csv, xlsx, json, eller html.
+    Dersom filen ikke er lagret lokalt må man bruke funksjonen download_temp_file
+    før denne funksjonen kan brukes. Bruk isåfall da tmp_file som filename for denne funksjonen.
+    
+    Funksjonen printer:
+    - Minne informasjon
+    - Kolonne navn
+    - Antall verdier
+    - Kolonne typer
+    - Nan verdier
+    - Nan prosent (om antall nanverdier overskrider nanlimit)
+    """
+
     _, extension = os.path.splitext(filename)
 
     # A test to check if the file exists in the directory.
@@ -28,91 +61,133 @@ def data_reader(filename):
         elif extension.lower()=='.json':
             data = pd.read_json(filename)
         elif extension.lower()=='.html':
-            data = pd.read_html(filename)
+            data = pd.read_html(filename)[0] #Dersom html filen bare har en tabell
         else:
             print('Filetype not valid')
             sys.exit()
         end_time = time.time()
         elapsed_time = end_time - start_time
         print(f"The dataset took: {elapsed_time:.2f} seconds to read")
-        print('--------------------------------------------------------------')
+        print(' ')
+
     except FileNotFoundError:
         print(f"Error: The file '{filename}' was not found. Please ensure it exists in the directory.")
         sys.exit()
 
+    #Reading the datastructure:
     try:
         print(f'Following information about the dataset {filename} was found:')
-        print('--------------------------------------------------------------')
+        print('--------------------------------------------------------------------------')
 
-        #Checks the size of the file
+        #Checks the size of the file in kB
         print('Memory information:')
+        print('-------------------')
         file_size = os.path.getsize(filename)
         print(f"Filesize: {file_size*10**(-3)} kB")
-        # Skriv ut minnebruk for DataFrame (i bytes)
         mem_usage = data.memory_usage(deep=True).sum()
         print(f"DataFrame size: {mem_usage*10**(-3)} kB")
+        print('  ')
+
+        columns_info = pd.DataFrame({
+            "Column": data.columns,
+            "Count": [len(data[col]) for col in data.columns],
+            "Type": [", ".join(data[col].dropna().apply(lambda x: type(x).__name__).unique()) if data[col].dtype == object else str(data[col].dtype)for col in data.columns],
+            "First element": [data[col].iloc[0] for col in data.columns]
+        })
+        print("Available data of the DataFrame:")
+        print("-" * 40)
+        print(columns_info.to_string(index=False))
+        print('   ')
+
+        # Converts objects of type float64 to float 32 to save memory usage
+        for col in data.columns:
+            if data[col].dtype == np.float64:
+                data[col] = data[col].astype(np.float32)
+                print(f"Column {col} converted to float32 to save memory")
+                print('DataFrame size is now: ', round(data.memory_usage(deep=True).sum() * 10 ** (-3), 2), 'kB')
+                print('   ')
+
+        #Tries to convert dataelements in the column to same type if the column consists of more dtypes than one
+        for col in data.columns:
+            unique_types = data[col].dropna().apply(lambda x: type(x)).unique() #Finsd unique dtypes of column
+
+            if len(unique_types) > 1:
+                print(f"Column '{col}' has mixed types: {[t.__name__ for t in unique_types]}")
+                try:
+                    #Tries to convert all elements to numeric value
+                    converted = pd.to_numeric(data[col], errors='raise')
+                    data[col] = converted
+                    print(f"Column '{col}' successfully converted to {data[col].dtype}")
+
+                except Exception as e:
+                    #if it doesnt work, convert all elements to strings
+                    print('  ')
+                    print(f"Could not convert '{col}' elements to numeric : {e}")
+                    # Dersom konvertering til numerisk ikke fungerer, konverter alle verdier til string
+                    data[col] = data[col].astype(str)
+                    print(f"All elements in '{col}' converted to string")
+                    print('   ')
+
 
         print('-------------------------------------------------------------')
 
-        #Skriv ut kolonnenavn (headers)
-        print("The DataFrame has the following data avaiable:")
-        print(*map(lambda col: f"{col}, with {len(data[col])} objects", data.columns), sep="\n")
-
-
-        print('-------------------------------------------------------------')
         #Checks the dataset for nan values
         check_nan = data.isnull().sum()
         print('Number of NaN values found:\n', check_nan)
+        nan_prosent=check_nan/len(data)*100
 
-    except AttributeError:
-        print('AttributeError: Check if the dataset is converted correctly to a DataFrame')
-        sys.exit()
+        if (nan_prosent > nanlimit).any():
+            print(' ')
+            print(f"The following columns exceed the NaN limit of {nanlimit}%")
+            print(round(nan_prosent[nan_prosent > nanlimit],2))
+            print('WARNING! Please check the quality of your datasource')
 
+
+    except Exception as e:
+        raise Exception(f"An error occurred while processing the data: {e}")
+
+    endTime = time.time()
+    tot_time = endTime - startTime
+    print(' ')
+    print('----------------------------------------------------------------')
+    print('The data reader code took ', tot_time, ' seconds to run')
     return data
 
-Data=data_reader(file_name)
+
+test=1
+                           ###TEST 1 AV FUNKSJONEN HER (der download_temp_file() funksjonen brukes først)###
+
+if test==1:
+    # URL til filen
+    csv_url = "https://sdi.eea.europa.eu/webdav/datastore/public/eea_t_national-emissions-reported_p_2024_v01_r00/CSV/UNFCCC_v27.csv"
+
+    # Last ned filen lokalt midlertidig:
+    temp_file = download_temp_file(csv_url)
+    print(f"Temporary file saved as: {temp_file}")
+
+    #Leser informasjon om datasettet
+    Data = data_reader(temp_file,10)
+
+    #Sletter den midlertidige filen:
+    os.remove(temp_file)
+    print(f"Temporary file {temp_file} deleted.")
+
+
+                            ### TEST 2 AV FUNKSJONEN HER (MED ALLEREDE NEDLASTET CSV FIL)
+elif test==2:
+
+    #File path
+    file_name = '2010_2020_rainfall.csv'
+
+    # Leser informasjon om datasettet
+    Data = data_reader(file_name, 10)
+
+else:
+    print('Test doesnt exist')
 
 
 
 
-
-
-"""
-#laster inn 2010 data (bruker list comperhensive her)
-data_2010_rain = data[data['date'].str.startswith('2010')]['rain'].tolist()
-data_2010_snow= data[data['date'].str.startswith('2010')]['snowfall'].tolist()
-
-#Legger sammen regn og snøfalldata for å lage en nedbørsliste
-data_2010_nedbor=list(map(lambda x, y: x+y, data_2010_rain, data_2010_snow))
-Nedbor_sortert=sorted(data_2010_nedbor)
-print(type(Nedbor_sortert))
-print(type(data_2010_rain))
-
-plt.plot(Nedbor_sortert, linestyle='-')
-plt.xlabel("Time (År)")
-plt.ylabel("Nedbør [mm]")
-plt.title("Totalt nedbør for Ørsta i 2010")
-plt.grid(True)
-plt.show()
-
-def Nedbor_data(data_nedbor):
-    total_nedbor=sum(data_nedbor)
-    nedbor_med_max=max(data_nedbor)
-    nedbor_mengde=[]
-    for i in data_nedbor:
-        if i!=0:
-            nedbor_mengde.append(i)
-    nedbor_prosent=len(nedbor_mengde)/len(data_nedbor)*100
-    nedbor_snitt=total_nedbor/len(data_nedbor)
-    print('Tot nebørsmengde i 2010: ', round(total_nedbor,2), 'mm')
-    print('Max nedbørmengde per time: ', nedbor_med_max, 'mm')
-    print(f'Det regnet {round(nedbor_prosent,2)}% av hele året i 2010 i Ørsta')
-    print(f'Snittet på nedbørmengde i 2010 var: {round(nedbor_snitt,2)} mm/h')
-    return
-
-Nedbor_data(Nedbor_sortert)
-
-"""
 
 
 
