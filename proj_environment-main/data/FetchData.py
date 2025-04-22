0f0942d43cc1189f9562258311093cc51ae1e9e1
rebekklj <rebekklj@stud.ntnu.no>
2025-04-22 17:40:41 +0200

    Scriptet FetchData.py Laster nå inn data for alle 5 årene 2020-2024.

--- proj_environment-main/data/FetchData.py
+++ proj_environment-main/data/FetchData.py
@@ -4,7 +4,7 @@ import sys
 
 script_dir= os.path.dirname(os.path.abspath(__file__)) #dir til dette scriptet
 project_dir=os.path.dirname(script_dir)#dir til hele prosjektmappen
-airdata_dir=os.path.join(script_dir,'airdata_excel')
+airdata_dir=os.path.join(script_dir,'airdata_excel') #dir til excelarkene
 sys.path.insert(0, project_dir)
 
 from Mappe_del1.Datareader import data_reader
@@ -21,14 +21,13 @@ print(f"Temporary file saved as: {temp_file}")
 
 
 AirData = FF.EU_AirPollutantsData(
-    startdate="2024-01-01T00:00:00Z",
-    enddate  ="2024-01-31T23:00:00Z",
+    startdate="2020-01-01T00:00:00Z",
+    enddate  ="2024-12-31T00:00:00Z",
     pollutants=["PM10", "NO2", "PM2.5","O3","SO2","CO"]
 )
 
 FF.write_to_excel_by_pollutant(AirData, out_dir="airdata_excel")
-
-file=os.path.join(airdata_dir,'CO.xlsx')
+file=os.path.join(airdata_dir,'NO2.xlsx')
 data_reader(file,20)
 
 
